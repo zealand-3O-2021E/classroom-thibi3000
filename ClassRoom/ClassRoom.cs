eb029ba78d31dce4b06176fd86b797c7d03ae046
thibo <thibo@LAPTOP-1FMMDL4N>
2021-09-02 10:37:04 +0200

    Task 2

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class ClassRoom
+    {
+        private string className;
+        private List<string> classList;
+        private DateTime semesterStart;
+
+        public ClassRoom()
+        {
+            className = "3o";
+            classList = new List<string>();
+            semesterStart = new DateTime(2021, 9, 2);
+        }
+
+
+    }
+
+
+}
