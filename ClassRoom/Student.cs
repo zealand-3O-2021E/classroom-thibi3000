d69510eb441db720e0ec5a137fccf2d87f185de9
thibo <thibo@LAPTOP-1FMMDL4N>
2021-09-02 10:26:15 +0200

    Task 1

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        private string name;
+        private int birthMonth;
+        private int birthday;
+
+        public Student(string name, int birthMonth, int birthday)
+        {
+            this.name = name;
+            this.birthMonth = birthMonth;
+            this.birthday = birthday;
+        }
+    }
+}
